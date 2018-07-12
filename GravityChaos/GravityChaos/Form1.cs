62b8257b3325df69e5c142c6109a3c717d56f880
ryan <email>
2018-07-11 19:55:27 -0500

    importing previous work from Experiments repository

---
+++ GravityChaos/GravityChaos/Form1.cs
@@ -0,0 +1,258 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using GravityChaos;
+using System.Media;
+
+
+namespace Form1
+{
+    public partial class Form1 : Form
+    {
+        // this is a list of all the particles we will use to make the gravity map
+        private List<Particle> Particles;
+
+
+        //------------------------------------------------------------------
+        // Define bitmap size for rendering the image of gravity chaos
+        //------------------------------------------------------------------
+        int ImageHeight;
+        int ImageWidth;
+        // create new bitmap to which our image will be printed
+        Bitmap Image;
+        // this is the name of the file to which the final image is stored
+        string outputFileName;
+        // this lets us know if we need to continue updating the image
+        bool imageComplete;
+        // x and y keep track of which pixel we are testing.
+        // The pixels of the screen are evaluated from left to right, then
+        // from top to bottom, like reading a book in english.
+        int x;
+        int y;
+        int ScreenRefreshPeriodMs;
+
+
+        //------------------------------------------------------------------
+        // Define space for particles
+        //------------------------------------------------------------------
+        double AspectRatio;
+        double SpaceHeight;
+        double SpaceWidth;
+        // calculates the scaling factor to translate between image space to particle space
+        double ImageToParticleSpace(int pixel)
+        {
+            return (double)(pixel * SpaceHeight / ImageHeight);
+        }
+
+
+        //------------------------------------------------------------------
+        // Add stationary particles that will act as targets
+        //------------------------------------------------------------------
+        double ParticleRadius;
+        double ParticleMass;
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.Particles = new List<Particle> { };
+
+
+            //------------------------------------------------------------------
+            // Define space for particles
+            //------------------------------------------------------------------
+            AspectRatio = 16.0 / 9.0;
+            SpaceHeight = 720;
+            SpaceWidth = SpaceHeight * AspectRatio;
+
+
+            //------------------------------------------------------------------
+            // Define bitmap size for rendering the image of gravity chaos
+            //------------------------------------------------------------------
+            ImageHeight = 2160;
+            //ImageHeight = 300;
+            ImageWidth = (int)(ImageHeight * AspectRatio);
+            // create new bitmap to which our image will be printed
+            Image = new Bitmap(ImageWidth, ImageHeight);
+            // where to save final image
+            outputFileName = "output.png";
+            // we need to generate the image
+            imageComplete = false;
+            // x and y keep track of which pixel we are testing.
+            // The pixels of the screen are evaluated from left to right, then
+            // from top to bottom, like reading a book in english.
+            x = 0;
+            y = 0;
+            ScreenRefreshPeriodMs = 250;
+
+
+            //------------------------------------------------------------------
+            // Add stationary particles that will act as targets
+            //------------------------------------------------------------------
+            ParticleRadius = SpaceHeight / 10.0;
+            ParticleMass = Math.Pow(ParticleRadius, 2.0);
+
+
+            //------------------------------------------------------------------
+            // Add one particle that will be the projectile (free to move)
+            //------------------------------------------------------------------
+            this.Particles.Add(new Particle { Color = Color.White });
+
+
+            
+
+            // stationary particle 1
+            this.Particles.Add(
+                new Particle
+                {
+                    Color = Color.Red,
+                    PositionX = SpaceWidth / 3.0,
+                    PositionY = SpaceHeight / 2.0,
+                    Fixed = true,
+                    Mass = ParticleMass
+                }
+            );
+            // stationary particle 2
+            this.Particles.Add(
+                new Particle
+                {
+                    Color = Color.Blue,
+                    PositionX = SpaceWidth * 2.0 / 3.0,
+                    PositionY = SpaceHeight / 2.0,
+                    Fixed = true,
+                    Mass = ParticleMass
+                }
+            );
+            // stationary particle 3
+            this.Particles.Add(
+                new Particle
+                {
+                    Color = Color.Lime,
+                    PositionX = SpaceWidth,
+                    PositionY = SpaceHeight,
+                    Fixed = true,
+                    Mass = ParticleMass*2
+                }
+            );
+            // stationary particle 3
+            this.Particles.Add(
+                new Particle
+                {
+                    Color = Color.Magenta,
+                    PositionX = SpaceWidth / 4,
+                    PositionY = SpaceHeight / 4,
+                    Fixed = true,
+                    Mass = ParticleMass / 2
+                }
+            );
+
+
+            this.DoubleBuffered = true;
+        }
+
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            // draw the new position of the particles on the screen
+            //
+            e.Graphics.DrawImage(Image, 0, 0);
+            Particle.Draw(Particles, e.Graphics);
+            this.Text = "GravityChaos: Drawing... " + (y * 100 / ImageHeight) + "% complete";
+        }
+
+
+        // this timer happens every millisecond. This is my workaround to
+        // always get control back to the map-generating routine.
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (!imageComplete)
+            {
+                // record when you entered the loop
+                DateTime TimeOfEntry = DateTime.Now;
+                int ElapsedTimeMs;
+                // work on the map
+                do
+                {
+                    // start the particle out at the current <x, y> coordinates
+                    Particles[0].PositionX = ImageToParticleSpace(x);
+                    Particles[0].PositionY = ImageToParticleSpace(y);
+                    // set the moving particle's initial velocity to 0.
+                    Particles[0].VelocityX = 0;
+                    Particles[0].VelocityY = 0;
+
+                    // run the simulation until the moving particle hits one of the stationary particles
+                    // have a timeout to prevent the programming from going in an endless loop
+                    bool collision = false;
+                    int iterations = 0, iterations_max = 50;
+                    while ((!collision) && (iterations < iterations_max))
+                    {
+                        // check to see if the moving particle has collided with any of the others
+                        foreach (Particle p in Particles.GetRange(1, Particles.Count - 1))
+                        {
+                            collision = Particle.CollisionCheck(Particles[0], p);
+                            // if the particle collided, set the pixel to the appropriate color.
+                            if (collision)
+                            {
+                                Image.SetPixel(x, y, p.Color);
+                                break;
+                            }
+                        }
+                        // run the simulation for a little while
+                        Particle.Update(Particles, 30);
+                        iterations++;
+                    }
+                    // depending on which target our moving particle hits, color the <x, y> pixel accordingly
+                    if (!collision)
+                    {
+                        Image.SetPixel(x, y, Color.Black);
+                    }
+
+                    // increment x and y
+                    x++;
+                    // if you have gotten to the right edge of the bitmap,
+                    if (x >= ImageWidth)
+                    {
+                        x = 0;
+                        y++;
+                    }
+                    // if you have completed the image, close it.
+                    if (y >= ImageHeight)
+                    {
+
+                        Image.Save(outputFileName, System.Drawing.Imaging.ImageFormat.Png);
+                        imageComplete = true;
+                        try
+                        {
+                            SoundPlayer player = new SoundPlayer("gotmail.mp3");
+                            player.Play();
+                        }
+                        catch
+                        {
+                            // oh well.
+                        }
+                        
+                    }
+                    ElapsedTimeMs = (int)(DateTime.Now - TimeOfEntry).TotalMilliseconds;
+                }
+                // keep working on the map until you need to refresh the screen again
+                while ((ElapsedTimeMs < this.ScreenRefreshPeriodMs) && !imageComplete);
+
+                // force the screen to be redrawn
+                Invalidate();
+            }
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ GravityChaos/GravityChaos/GravityChaos.cs
@@ -0,0 +1,252 @@
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Drawing;
+
+namespace GravityChaos
+{
+    class Particle
+    {
+        // define parameters of the particle        // units
+        public double PositionX     { get; set; }   // meters
+        public double PositionY     { get; set; }   // meters
+        public double VelocityX     { get; set; }   // meters / second
+        public double VelocityY     { get; set; }   // meters / second
+        public double ForceX        { get; set; }   // newtons (kg * m / s^2)
+        public double ForceY        { get; set; }   // newtons (kg * m / s^2)
+        public double Mass          { get; set; }   // kilograms
+        public bool   Fixed         { get; set; }   // true/false. A fixed particle does not move.
+        public Color  Color         { get; set; }   // the color of the particle.
+        private bool DeleteMe       { get; set; }   // indicates whether or not the particle should be removed from the simulation.
+
+        // for simplicity, I normalize the gravitational constant to 1.
+        public const double G = 1;
+
+        // when you create a new Particle, these are the default values of the member variables.
+        public Particle()
+        {
+            this.PositionX = 0;
+            this.PositionY = 0;
+            this.VelocityX = 0;
+            this.VelocityY = 0;
+            this.ForceX = 0;
+            this.ForceY = 0;
+            this.Mass = 1;
+            this.Fixed = false;
+            this.Color = Color.Lime;
+            this.DeleteMe = false;
+        }
+
+
+        //======================================================================
+        // this function returns the radius of the particle
+        //======================================================================
+        public double Radius()
+        {
+            return Math.Sqrt(this.Mass);
+        }
+
+
+        //======================================================================
+        // calculates and updates the velocity and position of each of the
+        // [Particles] over the specified [Time]. The larger the value of [Time]
+        // you give, the greater the simulation "error" will be. Perhaps in the
+        // future I'll implement/find  a runge kutta extrapolation function to
+        // reduce the error. For, linear is fine.
+        //======================================================================
+        public static void Update(List<Particle> Particles, double Time)
+        {
+            //------------------------------------------------------------------
+            // calculate the net forces acting on all particles
+            //------------------------------------------------------------------
+            // return all particles' forces to zero before recalculating
+            foreach (Particle p in Particles)
+            {
+                p.ForceX = 0;
+                p.ForceY = 0;
+            }
+            // calculate net forces acting on all particles
+            int c = Particles.Count();
+            for (int p1 = 0; p1 < c-1; p1++)
+            {
+                for(int p2 = p1+1; p2 < c; p2++)
+                {
+                    CalculateForcesBetweenTwoParticlesAndSum(Particles, Particles[p1], Particles[p2]);
+                }
+            }
+
+            //------------------------------------------------------------------
+            // calculate the new velocity of each particle
+            //------------------------------------------------------------------
+            foreach (Particle p in Particles)
+            {
+                p.VelocityX += Time * p.ForceX / p.Mass;
+                p.VelocityY += Time * p.ForceY / p.Mass;
+            }
+
+            //------------------------------------------------------------------
+            // calculate the new positions of each particle
+            //------------------------------------------------------------------
+            foreach (Particle p in Particles)
+            {
+                if (!p.Fixed)
+                {
+                    p.PositionX += Time * p.VelocityX;
+                    p.PositionY += Time * p.VelocityY;
+                }
+            }
+        }
+
+
+        //======================================================================
+        // this calculates the forces between two particles, and adds those
+        // forces to the ForceX and ForceY components in both Particle objects.
+        //======================================================================
+        public static void CalculateForcesBetweenTwoParticlesAndSum(List<Particle> Particles, Particle p1, Particle p2)
+        {
+            // calculate the distance between the objects squared
+            double dist_squared =
+                Math.Pow(p1.PositionX - p2.PositionX, 2) +
+                Math.Pow(p1.PositionY - p2.PositionY, 2);
+
+            double force;
+            // calculate the magnitude of the force normally.
+            force = Particle.G * p1.Mass * p2.Mass / dist_squared;
+
+            // calculate the angle (relative to positive x direction, A.K.A. the i unit vector)
+            double angle = Math.Atan2(
+                p1.PositionY - p2.PositionY,
+                p1.PositionX - p2.PositionX);
+
+            // calculate the x- and y-components of the force.
+            double FX = Math.Cos(angle) * force;
+            double FY = Math.Sin(angle) * force;
+            // apply forces to particle 2
+            p2.ForceX += FX;
+            p2.ForceY += FY;
+            // forces are equal and opposite on particle 1
+            p1.ForceX += -FX;
+            p1.ForceY += -FY;
+        }
+
+
+        // this function will check all particles in the list.
+        // it resolves all collisions.
+        public static void CollisionCheckAndResolveAll(List<Particle> Particles)
+        {
+            bool FoundCollision;
+            do
+            {
+                FoundCollision = false;
+                // calculate net forces acting on all particles
+                int c = Particles.Count();
+                if(c > 1)
+                {
+                    for (int p1 = 0; ((p1 < c - 1) && (!FoundCollision)); p1++)
+                    {
+                        for (int p2 = p1 + 1; ((p2 < c) && (!FoundCollision)); p2++)
+                        {
+                            // if the particles touch, or are overlapping
+                            if (Particle.CollisionCheck(Particles[p1], Particles[p2]))
+                            {
+                                Particle.Collision(Particles, Particles[p1], Particles[p2]);
+                                FoundCollision = true;
+                            }
+                        }
+                    }
+                }
+            }
+            while (FoundCollision);
+        }
+
+
+        // this function will tell you if two particles are touching
+        public static bool CollisionCheck(Particle p1, Particle p2)
+        {
+            // calculate the distance between the objects squared
+            double dist_squared =
+                Math.Pow(p1.PositionX - p2.PositionX, 2) +
+                Math.Pow(p1.PositionY - p2.PositionY, 2);
+            // if the distance squared between the two objects is less than or equal to the
+            // sum of the squares of the two radii, then the two objects are touching.
+            return (dist_squared <= Math.Pow(p1.Radius() + p2.Radius(), 2));
+        }
+
+
+        // call this function when two particles collide!
+        private static void Collision(List<Particle> Particles, Particle p1, Particle p2)
+        {
+            // p1 becomes the combination of p1 and p2 in terms of mass, position, and momentum.
+            // (p1 + p2) -> p1
+
+            if(p1.Fixed)
+            {
+                p1.Mass += p2.Mass;
+                Particles.Remove(p2);
+            }
+            else if(p2.Fixed)
+            {
+                p2.Mass += p1.Mass;
+                Particles.Remove(p1);
+            }
+            // if neither particle is fixed,
+            else
+            {
+                // calculate the net position of the combined particle
+                double p1m = p1.Mass / (p1.Mass + p2.Mass);
+                double p2m = p2.Mass / (p1.Mass + p2.Mass);
+                p1.PositionX = p1m * p1.PositionX + p2m * p2.PositionX;
+                p1.PositionY = p1m * p1.PositionY + p2m * p2.PositionY;
+
+                // calculate net momentum from two particles
+                double momentum_x = p1.VelocityX * p1.Mass + p2.VelocityX * p2.Mass;
+                double momentum_y = p1.VelocityY * p1.Mass + p2.VelocityY * p2.Mass;
+
+                // combine the masses of the two particles
+                p1.Mass = p1.Mass + p2.Mass;
+
+                // calculate new velocity of combined particle
+                p1.VelocityX = momentum_x / p1.Mass;
+                p1.VelocityY = momentum_y / p1.Mass;
+
+                Particles.Remove(p2);
+            }
+        }
+
+
+
+        // this draws all the particles in the list on the graphics object.
+        public static void Draw(List<Particle> Particles, Graphics screen)
+        {
+            Pen myPen;
+            float x, y, w, h;
+            foreach (Particle p in Particles)
+            {
+                myPen = new Pen(p.Color);
+
+                // draw a circle for the "body" of the particle
+                x = (float)(p.PositionX - p.Radius());
+                y = (float)(p.PositionY - p.Radius());
+                w = (float)(2 * p.Radius());
+                h = (float)(2 * p.Radius());
+                screen.DrawEllipse(myPen, x, y, w, h);
+
+                // draw a line showing the direction of the force extered on the particle
+                double force_mag = Math.Sqrt(p.ForceX * p.ForceX + p.ForceY * p.ForceY);
+                double force_x_norm = p.ForceX / force_mag;
+                double force_y_norm = p.ForceY / force_mag;
+                screen.DrawLine(myPen, (float)p.PositionX, (float)p.PositionY, (float)(p.PositionX + 2 * p.Radius() * force_x_norm), (float)(p.PositionY + 2 * p.Radius() * force_y_norm));
+
+                // draw a line showing the direction the particle is moving
+                double velocity_mag = Math.Sqrt(p.VelocityX * p.VelocityX + p.VelocityY * p.VelocityY);
+                double velocity_x_norm = p.VelocityX / velocity_mag;
+                double velocity_y_norm = p.VelocityY / velocity_mag;
+                screen.DrawLine(myPen, (float)p.PositionX, (float)p.PositionY, (float)(p.PositionX + p.Radius() * velocity_x_norm), (float)(p.PositionY + p.Radius() * velocity_y_norm));
+
+
+                myPen.Dispose();
+            }
+        }
+
+    }
+}
